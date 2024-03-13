89b388a330eab780246b472e6f3b47d7517f3557
GenkaOk <2690324+GenkaOk@users.noreply.github.com>
2024-03-13 14:09:20 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh
 
+if [ -z "$SSH_PORT" ]; then
+    SSH_PORT=22
+fi
+
 eval $(ssh-agent -s)
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-ssh-keyscan $SSH_HOST >> ~/.ssh/known_hosts
+ssh-keyscan -p $SSH_PORT $SSH_HOST >> ~/.ssh/known_hosts
 chmod 644 ~/.ssh/known_hosts
 
 exec "$@"
